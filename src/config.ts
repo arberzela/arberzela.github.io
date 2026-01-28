c3162d6a1bf15bf4893d11b9c95bf7bcbcb534ac
Arber Zela <arber.zela21@gmail.com>
2026-01-28 16:29:31 +0100

    migration to astro

---
+++ src/config.ts
@@ -0,0 +1,30 @@
+export const siteConfig = {
+    title: "Arber Zela",
+    position: "Postdoctoral Researcher",
+    affiliation: "ELLIS Institute Tübingen",
+    email: "arber.zela (at) tue.ellis.eu",
+
+    // SEO
+    keywords: "minimal light",
+    description: "The Minimal Light is a simple and elegant jekyll theme for academic personal homepage.",
+
+    // Links
+    googleScholar: "https://scholar.google.com/citations?user=hD_6YioAAAAJ&hl=en",
+    cvLink: "/assets/files/curriculum_vitae.pdf",
+    githubLink: "https://github.com/arberzela",
+    linkedin: "https://www.linkedin.com/in/arber-zela-ba95a2145",
+    twitter: "https://x.com/zelaarber",
+
+    // Images
+    avatar: "/assets/img/arber_small.jpg",
+    favicon: "/assets/img/favicon.png",
+    faviconDark: "/assets/img/favicon-dark.png",
+
+    // Settings
+    enableFootnote: true,
+    autoDarkMode: true,
+    font: "Serif", // or "Sans Serif"
+
+    // Google Analytics
+    googleAnalytics: "UA-111540567-4",
+};
